e03a6e4fce50e92305d26b85385bae6fdfd5600e
shuto012 <rinGo0120X@gmail.com>
2022-10-14 16:14:59 +0900

    userモデル単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname,        presence: true
-  validates :first_name,      presence: true
-  validates :last_name,       presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana,  presence: true
+  validates :first_name,      presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/}
+  validates :last_name,       presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/}
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :last_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
   validates :birth_day,       presence: true
-  
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+
+
 end
---
+++ spec/factories/user.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password}
+    password_confirmation {password}
+    first_name            {'山田'}
+    last_name             {'太郎'}
+    first_name_kana       {'ヤマダ'}
+    last_name_kana        {'タロウ'}
+    birth_day             {'1999-01-26'}
+  end
+end
