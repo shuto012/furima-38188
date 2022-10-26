2816aafda4fcaba332dafbf0910adc84d5ccbce3
shuto012 <rinGo0120X@gmail.com>
2022-10-26 14:45:16 +0900

    beforeまとめ、戻るボタン実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only:[:new, :edit]
+  before_action :find_item, only:[:show, :edit, :update]
   before_action :move_to_index, only:[:edit]
+
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -20,15 +22,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -41,11 +40,16 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:item_name, :comment, :price, :category_id, :condition_id, :prefecture_id, :delivery_charge_id, :day_to_ship_id, :image).merge(user_id: current_user.id)
   end
+
   def move_to_index
-    item = Item.find(params[:id])
-    if item.user_id != current_user.id
+    if @item.user_id != current_user.id
       redirect_to action: :index
     end
   end
+
+  def find_item
+    @item = Item.find(params[:id])
+  end
+
   
 end
