e5f1dbeb36c5da31a439b6583539ede39e030f2b
shuto012 <rinGo0120X@gmail.com>
2022-10-26 13:50:57 +0900

    updateアクション編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,14 +26,15 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  def updata
-    @item = Item.find(params[:id])
-    if @item.update(item_params)
-      redirect_to item_path(@item.id)
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      redirect_to item_path(item.id)
     else
       render :edit
     end
   end
+
 
   private
   def item_params
