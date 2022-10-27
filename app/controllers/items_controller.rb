01b85181c50619483eb3e175108cb192d41785c6
shuto012 <rinGo0120X@gmail.com>
2022-10-27 12:49:55 +0900

    beforeアクション編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only:[:new, :edit, :destroy]
   before_action :find_item, only:[:show, :edit, :update]
-  before_action :move_to_index, only:[:edit, :update, :destroy]
+  before_action :move_to_index, only:[:edit, :update]
 
 
   def index
