61a97245f8ff49fb8bae2beb13c93136b642f221
shuto012 <rinGo0120X@gmail.com>
2022-10-27 12:45:32 +0900

    ログイン中のみ削除可能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only:[:new, :edit]
+  before_action :authenticate_user!, only:[:new, :edit, :destroy]
   before_action :find_item, only:[:show, :edit, :update]
-  before_action :move_to_index, only:[:edit]
+  before_action :move_to_index, only:[:edit, :update, :destroy]
 
 
   def index
