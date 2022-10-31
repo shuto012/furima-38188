d660a4545132e2632dba2cddd39ffb4dbe0a030a
shuto012 <rinGo0120X@gmail.com>
2022-10-31 15:59:34 +0900

    バリデーション

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,10 @@
 class Address < ApplicationRecord
+  belongs_to :order
+
+  validates :post_code,            presence: true
+  validates :prefecture_id,        presence: true
+  validates :municipality,         presence: true
+  validates :address,              presence: true
+  validates :phone_number,         presence: true
+  
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,5 +3,4 @@ class Order < ApplicationRecord
   belongs_to :item
   has_one :address
 
-  
 end
