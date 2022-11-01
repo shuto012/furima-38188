a660108d660d44c8b635deb7facc0e1b1b7bfca9
shuto012 <rinGo0120X@gmail.com>
2022-11-01 14:05:25 +0900

    Formオブジェクト作成

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,10 +1,4 @@
 class Address < ApplicationRecord
   belongs_to :order
 
-  validates :post_code,            presence: true, format: { with: /\A\d{3}[-]\d{4}\z/}
-  validates :prefecture_id,        presence: true, numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :municipality,         presence: true
-  validates :address,              presence: true
-  validates :phone_number,         presence: true, format: { with: /\A\d{10,11}\z/}
-  
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,4 +1,22 @@
 class OrderAddress
   include ActiveModel::Model
   attr_accessor :user, :item, :post_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :order
+
+  with_options presence: true do
+    validates :post_code,          format: { with: /\A\d{3}[-]\d{4}\z/}
+    validates :prefecture_id,        numericality: { other_than: 1 , message: "can't be blank"} 
+    validates :municipality
+    validates :address
+    validates :phone_number,         format: { with: /\A\d{10,11}\z/}
+    validates :user_id
+    validates :item_id
+    validates :order_id
+    validates :address_id
+  end
+  validates :building_name
+
+  def save
+    order = Order.create(item_id: item_id, user_id: user_id)
+    Address.create(post_code: post_code, prefecture: prefecture, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number,order_id: order.id)
+  end
 end
