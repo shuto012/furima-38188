78e2c9abb3c6ab7a6f49f81da8bedf7f032eb73b
shuto012 <rinGo0120X@gmail.com>
2022-11-05 19:53:59 +0900

    エラー回避

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe OrderAddress, type: :model do
     user = FactoryBot.create(:user)
     item = FactoryBot.create(:item)
     @order_address = FactoryBot.build(:order_address, user_id: user.id, item_id: item.id)
+    sleep 0.1
   end
 
   describe '配送先情報の保存' do
