8b427d5d21bb5b573274bb9eb155a3345e6a4abd
shuto012 <rinGo0120X@gmail.com>
2022-11-05 14:09:57 +0900

    決済機能修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,9 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :purchase_item, only:[:index, :create]
 
   def index
     @order_address = OrderAddress.new
-    @item = Item.find(params[:item_id])
   end
 
   def create
@@ -26,10 +26,13 @@ class OrdersController < ApplicationController
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
-      amount: order_params[:price],  # 商品の値段
+      amount: @item.price,  # 商品の値段
       card: order_params[:token],    # カードトークン
       currency: 'jpy'                 # 通貨の種類（日本円）
     )
   end
 
+  def purchase_item
+    @item = Item.find(params[:item_id])
+  end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :order_id, :address_id, :token
+  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :order_id, :token
 
   with_options presence: true do
     validates :post_code,          format: { with: /\A\d{3}[-]\d{4}\z/}
@@ -10,14 +10,12 @@ class OrderAddress
     validates :phone_number,         format: { with: /\A\d{10,11}\z/}
     validates :user_id
     validates :item_id
-    validates :order_id
-    validates :address_id
 
     validates :token
   end
 
   def save
-    order = Order.create(item_id: item_id, user_id: current_user.id)
-    Address.create(post_code: post_code, prefecture: prefecture, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number,order_id: order.id)
+    order = Order.create(item_id: item_id, user_id: user_id)
+    Address.create(post_code: post_code, prefecture_id: prefecture_id, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number,order_id: order.id)
   end
 end
